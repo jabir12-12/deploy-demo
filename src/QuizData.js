27f3c6aea9272cd6723d14d908ce590cbff2f7c7
Jabir Ali <146981757+jabir12-12@users.noreply.github.com>
2025-04-17 13:06:58 +0530

    Update QuizData.js

--- src/QuizData.js
+++ src/QuizData.js
@@ -1,7 +1,7 @@
 const quizData = [
     {
-        question: "What was the first video game ever made?",
-        options: ["Pong", "Spacewar!", "Tetris", "Computer Space"],
+        question: "What is the CEO of amazon?",
+        options: ["Pong", "Spacewar!", "Tetris", "Jeff Bezzos"],
         answer: "Spacewar!"
     },
     {
